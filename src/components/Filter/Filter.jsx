f91b12916f06ff793c0c894fe9b76083c159b0a4
Michal Leszczyk <Michal Leszczyk>
2023-10-04 18:48:26 +0200

    filter change

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,17 +1,13 @@
-import { Component } from 'react';
 import css from './Filter.module.css';
 import PropTypes from 'prop-types';
-export class Filter extends Component {
-  render() {
-    const { filter, setFilter } = this.props;
-    return (
-      <div className={css.filterWrapper}>
-        <p>Find contacts by name</p>
-        <input type="text" value={filter} onChange={setFilter}></input>
-      </div>
-    );
-  }
-}
+export const Filter = ({ filter, setFilter }) => {
+  return (
+    <div className={css.filterWrapper}>
+      <p>Find contacts by name</p>
+      <input type="text" value={filter} onChange={setFilter}></input>
+    </div>
+  );
+};
 Filter.propTypes = {
   filter: PropTypes.string,
   setFilter: PropTypes.func,
